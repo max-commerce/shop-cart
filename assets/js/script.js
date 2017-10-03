606b086b4cbefd98cbf0d7d563bf866cb9a8dcea
num <numkms@gmail.com>
2017-10-04 04:13:39 +1000

    ajax update default cart widget

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,11 +1,21 @@
 var AddToCartSuccessCallback = function(data){
     maxCommAddToCartSuccess(data);
 };
+var cartLinkReload = function(data){
+    $('.maxcom-cart-link-total').each(function(){
+            $(this).html(data.total);
+    });
+    $('.maxcom-cart-link-count').each(function(){
+        $(this).html(data.products_count);
+    });
+
+};
 
 $(document).on('submit','.max-comm-add-to-cart-form',function(e){
     e.preventDefault();
 
     $.post($(this).attr('action'),$(this).serialize(),function(data){
             AddToCartSuccessCallback(data);
+            cartLinkReload(data);
     });
 });
