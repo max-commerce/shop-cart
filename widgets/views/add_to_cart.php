da6c2ede60729f83114773dfd2a2a0dab156ea41
num <numkms@gmail.com>
2017-10-04 02:13:48 +1000

    bind on class not id for each button

--- widgets/views/add_to_cart.php
+++ widgets/views/add_to_cart.php
@@ -5,22 +5,26 @@ use yii\widgets\ActiveForm;
 
 $btnText = $this->context->btnText ? $this->context->btnText : 'Купить';
 $btnOptions = $this->context->btnOptions ? $this->context->btnOptions : ['class' => 'btn btn-primary'];
-
+//Service class for working with form from JS
+$formClass = 'max-comm-add-to-cart-form';
 $formId = 'addToCart-form-' . $model->id;
 //Not sure about this (cause each button will register script but for now nice)
 if($this->context->ajax){
 	$this->registerJs("
-		$(document).on('submit','#".$formId."',function(e){
+		$(document).on('submit','.".$formClass."',function(e){
 			e.preventDefault();
 			$.post($(this).attr('action'),$(this).serialize(),function(data){
 				".$this->context->ajaxResultCallback."
 			});
 		});	
-	");
+	",\yii\web\View::POS_READY);
 }
 
 $form = ActiveForm::begin([
     'id' => $formId,
+	'options' => [
+		'class' => $formClass,
+	],
     'method' => 'post',
     'action' => '/cart/add-item'
 ]) ?>
